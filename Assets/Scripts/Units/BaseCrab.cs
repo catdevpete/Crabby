aacddc1c059a1ea0f1a19619b18adf91a4fc698a
Scott Tatham <scottytatham@gmail.com>
2016-10-22 00:54:44 +1100

    Scott' MVP stuff.

--- Assets/Scripts/Units/BaseCrab.cs
+++ Assets/Scripts/Units/BaseCrab.cs
@@ -12,23 +12,36 @@ public class BaseCrab : MonoBehaviour
     [SerializeField]
     GameObject enemyBase;
 
+    bool canAtt;
     int team;
-    float health;
+    float hp;
     float att;
     float mS;
+    float aS;
     float range;
     State cState;
+    GameObject target;
     Collider[] cols;
 
     public int GetTeam() { return team; }
+    public float GetHp() { return hp; }
+    
+    public void SetHp(float _hp) { hp = _hp; }
 
     void Start()
     {
-        
+        canAtt = true;
+        mS = 3;
+        aS = 1;
+        range = 2;
+        cState = State.MOVE;
     }
 
     void Update()
     {
+        Death();
+        Detect();
+
         switch (cState)
         {
             case State.MOVE:
@@ -45,6 +58,15 @@ public class BaseCrab : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter(Collision _col)
+    {
+        if (_col.gameObject.GetComponent<CrabNest>() != null)
+        {
+            _col.gameObject.GetComponent<CrabNest>().SetHp(_col.gameObject.GetComponent<CrabNest>().GetHp() - 1);
+            Destroy(gameObject);
+        }
+    }
+
     void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, enemyBase.transform.position, mS * Time.deltaTime);
@@ -52,7 +74,24 @@ public class BaseCrab : MonoBehaviour
 
     void Attack()
     {
+        if (target == null)
+        {
+            cState = State.MOVE;
+        }
 
+        if (canAtt)
+        {
+            target.GetComponent<BaseCrab>().SetHp(target.GetComponent<BaseCrab>().GetHp() - att);
+            StartCoroutine(AttCycle(aS));
+        }
+    }
+
+    void Death()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Detect()
@@ -63,8 +102,16 @@ public class BaseCrab : MonoBehaviour
         {
             if (c.GetComponent<BaseCrab>() != null && c.GetComponent<BaseCrab>().GetTeam() != team)
             {
+                target = c.gameObject;
                 cState = State.ATTACK;
             }
         }
+    }
+
+    IEnumerator AttCycle(float _t)
+    {
+        canAtt = false;
+        yield return new WaitForSeconds(_t);
+        canAtt = true;
     }
 }
---
+++ Assets/Scripts/Units/CrabNest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrabNest : MonoBehaviour
+{
+    float hp;
+
+    public float GetHp() { return hp; }
+
+    public void SetHp(float _hp) { hp = _hp; }
+
+    void Start()
+    {
+        hp = 1;
+    }
+
+    void Update()
+    {
+        Lose();
+    }
+
+    void Lose()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+            // GG;
+        }
+    }
+}
