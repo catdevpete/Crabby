b048e8af933b402c2713cea208dd33fc5594e04a
Peter Liang <Peterdhl88@gmail.com>
2016-10-22 00:24:27 +1100

    Added pausing to crab wand Added commented out section for spawning crabs from bases Added Game Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public string winScene;
+	public string loseScene;
+
+	public void TriggerWin()
+	{
+		SceneManager.LoadScene(winScene);
+	}
+
+	public void TriggerLose()
+	{
+		SceneManager.LoadScene(loseScene);
+	}
+
+	/*
+	 * 
+	void Update()
+	{
+		if ()
+		{
+
+		}
+		else if ()
+		{
+
+		}
+		else
+		{
+
+		}
+	}
+	*/
+}
