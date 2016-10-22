8cd29ad4033ed1d3c79c107c5c16ffe077191ef0
Scott Tatham <scottytatham@gmail.com>
2016-10-22 12:09:35 +1100

    Ditto.

---
+++ Assets/Scripts/Units/Crusketeer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crusketeer : BaseCrab
+{
+    protected override void Start()
+    {
+        canAtt = true;
+        hp = 8;
+        att = 3;
+        mS = 3;
+        aS = 2;
+        range = 2;
+        cState = State.MOVE;
+    }
+}
